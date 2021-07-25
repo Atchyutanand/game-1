4e6258e61e934ca95e2aff763473c668f76e028a
Atchyutanand <78865290+Atchyutanand@users.noreply.github.com>
2021-07-25 11:58:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var PLAY=1;
+var END=2;
+var gameState=PLAY;
+var ;
+var man,virus,start,reset,gameOver,playAgain;
+
+function preload(){
+    man=loadAnimation("man running 1.png","man running 2.png" );
+
+    virus=loadImage("corona.png")
+    
+}
